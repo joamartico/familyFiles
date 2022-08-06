49efc31a29ac092974f95056344e930e5ead4fd0
joamartico <joamartico@gmail.com>
2022-08-06 11:05:30 -0300

    server.js (now works offline?)

--- pages/_app.js
+++ pages/_app.js
@@ -45,10 +45,7 @@ function MyApp({ Component, pageProps }) {
 				/>
 				<meta name="mobile-web-app-capable" content="yes" />
 				<meta name="apple-mobile-web-app-capable" content="yes" />
-				<meta
-					name="apple-mobile-web-app-status-bar-style"
-					content="default"
-				/>
+				<meta name="apple-mobile-web-app-status-bar-style" content="white" />
 				
 			</Head>
 			<Context>
