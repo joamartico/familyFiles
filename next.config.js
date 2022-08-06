c45ee1f6bf97f706f4f4fab3726eac3718df7d26
joamartico <joamartico@gmail.com>
2022-08-06 10:58:07 -0300

    register, runtimeCaching, skipWaiting and disable on development

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ module.exports = withPWA({
   
   pwa: {
     dest: '/public',
-    // register: true,
-    // skipWaiting: true,
-    // disable: process.env.NODE_ENV === 'development',
-    // runtimeCaching
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    runtimeCaching
   },
     
 
